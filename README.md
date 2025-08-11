9ce7f38c39bd5bfa122cd244af1c402f3282d029
abu-umair <mrdwnw93@gmail.com>
2025-08-11 11:58:45 +0700

    + mengecek di Postman dengan pilihan gRpc

--- README.md
+++ README.md
@@ -17,15 +17,20 @@ go get google.golang.org/grpc
 3. Run Server
 ```bash
 go run main.go
-
+```
 4. membuat koleksi proto (didalam folder proto, kemudian membuat service proto)
 
 5. Generate Proto
 ```bash
 protoc --go_out=./pb --go-grpc_out=./pb --proto_path=./proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative service/service.proto
-
+```
 6. Menggunakan .env, dan download package
 ```bash
 go get github.com/joho/godotenv
 
 ```
+7. Run Ulang Server
+```bash
+go run main.go
+```
+8. mengecek di Postman dengan pilihan gRpc
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/abu-umair/ecommerce-go-grpc-be/internal/handler"
 	"github.com/abu-umair/ecommerce-go-grpc-be/pb/service"
@@ -13,7 +14,7 @@ import (
 
 func main() { //?Sebagai gRpc server
 	godotenv.Load()
-	
+
 	lis, err := net.Listen("tcp", ":50051") //? mengembalikan 2 value yaitu listener dan error
 	if err != nil {
 		log.Panicf("Error when listening %v", err)
@@ -25,8 +26,10 @@ func main() { //?Sebagai gRpc server
 
 	service.RegisterHelloWorldServiceServer(serv, serviceHandler)
 
-	reflection.Register(serv)
-	log.Println("Reflection is registered")
+	if os.Getenv("ENVIRONMENT") == "dev" {
+		reflection.Register(serv)
+		log.Println("Reflection is registered")
+	}
 
 	log.Println("Server is running on :50051 port")
 	if err := serv.Serve(lis); err != nil {
