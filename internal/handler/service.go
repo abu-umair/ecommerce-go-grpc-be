be7c181746af3d71dbbc3be6debff71fd21b5d04
abu-umair <mrdwnw93@gmail.com>
2025-08-11 11:24:06 +0700

    + membuat template methode untuk service handler

--- internal/handler/service.go
+++ internal/handler/service.go
@@ -1,11 +1,20 @@
 package handler //*nama package handler
 
-import "github.com/abu-umair/ecommerce-go-grpc-be/pb/service"
+import (
+	"context"
+
+	"github.com/abu-umair/ecommerce-go-grpc-be/pb/service"
+)
 
 type IServiceHandler interface{}
 
 type serviceHandler struct {
 	service.UnimplementedHelloWorldServiceServer
+}
+
+// * membuat methode untuk service handler
+func (sh *serviceHandler) HelloWorld(ctx context.Context, request *service.HelloWorldRequest) (*service.HelloWorldResponse, error) {
+
 }
 
 func NewServiceHandler() IServiceHandler {
