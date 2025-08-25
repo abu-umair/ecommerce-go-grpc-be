41af97504af5837e6afb884297366c4a08e0a7df
abu-umair <mrdwnw93@gmail.com>
2025-08-25 14:35:46 +0700

    ~ delete panic (testing error)

--- internal/handler/service.go
+++ internal/handler/service.go
@@ -2,7 +2,6 @@ package handler //*nama package handler
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/abu-umair/ecommerce-go-grpc-be/pb/service"
@@ -14,7 +13,7 @@ type serviceHandler struct {
 
 // * membuat methode untuk service handler
 func (sh *serviceHandler) HelloWorld(ctx context.Context, request *service.HelloWorldRequest) (*service.HelloWorldResponse, error) {
-	panic(errors.New("not implemented")) //?testing error
+	// panic(errors.New("not implemented")) //?testing error
 	return &service.HelloWorldResponse{
 		Message: fmt.Sprintf("Hello %s", request.Name),
 	}, nil
