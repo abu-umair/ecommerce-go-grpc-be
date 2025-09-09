75792d3ecc9648dc26f6d40098a9f0e14fc4b17f
abu-umair <mrdwnw93@gmail.com>
2025-09-09 10:37:05 +0700

    + mengimplementasikan di auth.go

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -19,41 +19,63 @@ func (sh *authHandler) Register(ctx context.Context, request *auth.RegisterReque
 	if err != nil {
 		return nil, err
 	}
-	
+
 	if validationErrors != nil {
 		return &auth.RegisterResponse{
 			Base: utils.ValidationErrorResponse(validationErrors),
 		}, nil
 	}
-	
+
 	//?proses Register
 	res, err := sh.authService.Register(ctx, request)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return res, nil
 }
 
-//? mengimplementasikan auth service login
-func (sh *authHandler) Login(ctx context.Context, request *auth.LoginRequest) (*auth.LoginResponse, error) {
-validationErrors, err := utils.CheckValidation(request)
+// ? mengimplementasikan auth service login
+func (sh *authHandler) Logout(ctx context.Context, request *auth.LogoutRequest) (*auth.LogoutResponse, error) {
+	validationErrors, err := utils.CheckValidation(request)
 	if err != nil {
 		return nil, err
 	}
-	
+
+	if validationErrors != nil {
+		return &auth.LogoutResponse{
+			Base: utils.ValidationErrorResponse(validationErrors),
+		}, nil
+	}
+
+	//?proses Logout
+	res, err := sh.authService.Logout(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// ? mengimplementasikan auth service logout
+func (sh *authHandler) Login(ctx context.Context, request *auth.LoginRequest) (*auth.LoginResponse, error) {
+	validationErrors, err := utils.CheckValidation(request)
+	if err != nil {
+		return nil, err
+	}
+
 	if validationErrors != nil {
 		return &auth.LoginResponse{
 			Base: utils.ValidationErrorResponse(validationErrors),
 		}, nil
 	}
-	
+
 	//?proses Login
 	res, err := sh.authService.Login(ctx, request)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return res, nil
 }
 
