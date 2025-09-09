3570fa47e92e8bc3080967a8c4efb7b436d6a6aa
abu-umair <mrdwnw93@gmail.com>
2025-09-09 11:08:02 +0700

    + logic kembalikan token tadi hingga menjadi entity jwt

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -155,7 +155,26 @@ func (as *authService) Logout(ctx context.Context, request *auth.LogoutRequest) 
 	jwtToken := tokenSplit[1]
 
 	//? kembalikan token tadi hingga menjadi entity jwt
-	
+	tokenClaims, err := jwt.ParseWithClaims(jwtToken, &entity.JwtClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if t, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method %v", t.Header["alg"])
+		}
+
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	if !tokenClaims.Valid {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	var claims *entity.JwtClaims
+	if claims, ok = tokenClaims.Claims.(*entity.JwtClaims); !ok {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
 
 	//? kita masukkan token ke dalam memory db / cache
 
