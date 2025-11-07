eb295f05f6352ac5a1f2ee50ff49c5463f2ce8cd
abu-umair <mrdwnw93@gmail.com>
2025-11-07 14:44:54 +0700

    + menambahkan logic Cek terlebi dahulu apakah product id itu ada di db

--- internal/service/cart_service.go
+++ internal/service/cart_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 
+	"github.com/abu-umair/ecommerce-go-grpc-be/internal/repository"
+	"github.com/abu-umair/ecommerce-go-grpc-be/internal/utils"
 	"github.com/abu-umair/ecommerce-go-grpc-be/pb/cart"
 )
 
@@ -10,20 +12,33 @@ type ICartService interface {
 	AddProductToCart(ctx context.Context, request *cart.AddProductToCartRequest) (*cart.AddProductToCartResponse, error)
 }
 
-type cartService struct{}
+type cartService struct {
+	productRepository repository.IProductRepository
+}
 
 func (cs *cartService) AddProductToCart(ctx context.Context, request *cart.AddProductToCartRequest) (*cart.AddProductToCartResponse, error) {
 	//* cek terlebi dahulu apakah product id itu ada di db
+	productEntity, err := cs.productRepository.GetProductById(ctx, request.ProductId)
+	if err != nil {
+		return nil, err
+	}
+	if productEntity == nil {
+		return &cart.AddProductToCartResponse{
+			Base: utils.NotFoundResponse("Product not found"),
+		}, nil
+	}
 
 	//* cek ke db apakah product udah ada di cart user
 
-		//* udah ada -> update
+	//* udah ada -> update
 
-		//* belum -> insert
+	//* belum -> insert
 
 	//* response
 }
 
-func NewCartService() ICartService {
-	return &cartService{}
+func NewCartService(productRepository repository.IProductRepository) ICartService {
+	return &cartService{
+		productRepository: productRepository,
+	}
 }
