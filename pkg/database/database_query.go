e4783b7cb345b09ff3578767f0fddb75f5c61514
abu-umair <mrdwnw93@gmail.com>
2025-11-16 21:34:07 +0700

    * typo

--- pkg/database/database_query.go
+++ pkg/database/database_query.go
@@ -6,7 +6,7 @@ import (
 )
 
 type DatabaseQuery interface {
-	QueryContext(cx context.Context, query string, args ...any) (*sql.Rows, error)
-	ExecContext(cx context.Context, query string, args ...any) (*sql.Result, error)
-	QueryRowContext(cx context.Context, query string, args ...any) *sql.Row
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
 }
